0c7c32de399a3018f5498f992b4ea909b4de88f2
M Andriansyah Nur Cahya <20261732+andriansyahnc@users.noreply.github.com>
2026-07-17 07:49:29 +0700

    security: tighten CSP (connect/img/base-uri/object-src) + escape JSON-LD (#38)

--- components/seo/JsonLd.tsx
+++ components/seo/JsonLd.tsx
@@ -2,9 +2,20 @@ interface JsonLdProps {
   data: Record<string, unknown>
 }
 
+// Escape characters that could break out of the <script> tag or the JSON when the data ever
+// contains them (e.g. a post title with "</script>"). JSON.stringify alone does NOT escape these.
+function safeJsonLd(data: Record<string, unknown>): string {
+  return JSON.stringify(data)
+    .replace(/</g, '\\u003c')
+    .replace(/>/g, '\\u003e')
+    .replace(/&/g, '\\u0026')
+    .replace(/\u2028/g, '\\u2028')
+    .replace(/\u2029/g, '\\u2029')
+}
+
 export default function JsonLd({ data }: JsonLdProps) {
   return (
-    <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(data) }} />
+    <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: safeJsonLd(data) }} />
   )
 }
 
